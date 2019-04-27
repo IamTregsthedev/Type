13ad440e1489f8ae22ea4728f6d620965089a5d3
Arav Narula <proteximportsexports@gmail.com>
2019-04-27 13:15:44 -0400

    RED

--- type.js
+++ type.js
@@ -51,6 +51,6 @@
     
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ff0000}";
         document.body.appendChild(css);
     };
